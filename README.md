e64f6409d43d8ad17f8b624d8392d63a694026eb
Adam B <atomicbrod101@gmail.com>
2019-07-23 13:37:28 -0400

    Improved script & updated README.md

--- README.md
+++ README.md
@@ -1,35 +1,32 @@
-# Prometheus Network Speedtest Exporter
-Prometheus exporter for network speedtests using `speedtest-cli` command in Linux distros. Developed in Python3 language and has been tested on Ubuntu v18.04.
+# Prometheus Speedtest Exporter
+Prometheus exporter where it reports speedtest statistics based on user's preference.
 
 ## Requirements
-* Python v3+
-* Python Packages: `prometheus_client`
-* Speedtest-cli
+* Python v3
+* Python Packages: `prometheus_client speedtest-cli`
 
 ## Setup
-1. Install `speedtest-cli`.
-    * Ubuntu/Debian: `apt install speedtest-cli`
-    * CentOS/RHEL: `wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py; chmod +x speedtest-cli;`
+1. Install required Python packages
 2. Run program:
-```py
+```bash
 python3 speedtest_exporter.py
 ```
 
-## Command Arguments
-* `--web.listen-address="0.0.0.0:9100"` = Specify host and port to display metrics for scraping. (DEFAULT: 0.0.0.0:9100)
-* `--server_id` = Specify server by ID to perform speedtests on.
-* `--bind_ip` = Bind to specific source IP address to run speedtests from. (DEFAULT: Closest server to source will automatically be used.)
-* `--interval` = Set interval in seconds on how often data should be updated. (DEFAULT: 10 seconds)
+## Usage
+* `--web.listen-address="0.0.0.0:9100"` = Specify host and port for Prometheus to use to display metrics for scraping.
+* `--servers` = Specific a or list of server ID(s) by comma to perform speedtests with. You can find the ID through [here](https://www.speedtestserver.com/).
+* `--source` = Specify source IP for speedtest to use to perform test.
+* `--interval` = How often in seconds the tests should be performed.
 
 ## Metrics Example
 ```
-# HELP speedtest_upload Current network upload speed in bytes/sec.
-# TYPE speedtest_upload gauge
-speedtest_upload 102188975.87000838
-# HELP speedtest_download Current network download speed in bytes/sec.
-# TYPE speedtest_download gauge
-speedtest_download 305429320.9009878
-# HELP speedtest_ping Current ping time in milliseconds.
+# HELP speedtest_ping Ping time in milliseconds.
 # TYPE speedtest_ping gauge
-speedtest_ping 53.816
+speedtest_ping{server_id="1062",server_loc="United States",server_name="Mount Pleasant, MI"} 29.629
+# HELP speedtest_download Network download speed in bytes/sec.
+# TYPE speedtest_download gauge
+speedtest_download{server_id="1062",server_loc="United States",server_name="Mount Pleasant, MI"} 1.7703202804595023e+08
+# HELP speedtest_upload Network upload speed in bytes/sec.
+# TYPE speedtest_upload gauge
+speedtest_upload{server_id="1062",server_loc="United States",server_name="Mount Pleasant, MI"} 4.086002063756785e+06
 ```
--- speedtest_exporter.py
+++ speedtest_exporter.py
@@ -1,96 +1,83 @@
 #!/usr/bin/env python3
-
-from prometheus_client import start_http_server, Gauge
-from threading import Thread
-import subprocess
+# Imports
+import prometheus_client
+import traceback
+import speedtest
+import threading
 import argparse
-import json
 import time
 
-
 # Arguments
-parser = argparse.ArgumentParser(description='Prometheus exporter for network speedtests.')
-parser.add_argument('--web.listen-address', action='store', dest='listen_addr', help='Specify host and port to display metrics for scraping.')
-parser.add_argument('--server_id', action='store', dest='server_id', help='Specify server by ID to perform speedtests on.')
-parser.add_argument('--bind_ip', action='store', dest='bind_ip', help='Bind to specific source IP address to run speedtests from.')
-parser.add_argument('--interval', action='store', dest='interval', help='Set interval in seconds on how often data should be updated.')
-
+parser = argparse.ArgumentParser(description='Prometheus exporter where it reports speedtest statistics based on user\'s preference.')
+parser.add_argument('--web.listen-address', action='store', dest='listen_addr', help='Specify host and port for Prometheus to use to display metrics for scraping.')
+parser.add_argument('--servers', action='store', dest='servers', help='Specific a or list of server ID(s) by comma to perform speedtests with.')
+parser.add_argument('--source', action='store', dest='source', help='Specify source IP for speedtest to use to perform test.')
+parser.add_argument('--interval', action='store', dest='interval', help='How often in seconds the tests should be performed.')
 
 # Attributes
-speedtest_cmd = 'speedtest-cli --json --bytes'
-metrics_set = False
 metrics = {
-    'speedtest_ping': Gauge('speedtest_ping', 'Current ping time in milliseconds.'),
-    'speedtest_download': Gauge('speedtest_download', 'Current network download speed in bytes/sec.'),
-    'speedtest_upload': Gauge('speedtest_upload', 'Current network upload speed in bytes/sec.')
+    'speedtest_ping': prometheus_client.Gauge('speedtest_ping', 'Ping time in milliseconds.', ['server_name', 'server_loc', 'server_id']),
+    'speedtest_download': prometheus_client.Gauge('speedtest_download', 'Network download speed in bytes/sec.', ['server_name', 'server_loc', 'server_id']),
+    'speedtest_upload': prometheus_client.Gauge('speedtest_upload', 'Network upload speed in bytes/sec.', ['server_name', 'server_loc', 'server_id'])
 }
 
+# Classes
+class UpdateMetrics(threading.Thread):
+    def __init__(self, _servers, _source, _interval):
+        threading.Thread.__init__(self)
+        self.servers = _servers
+        self.interval = _interval
+    
+    def run(self):
+        while True:
+            try:
+                print('INFO: Updating metrics...', flush=True)
+                # Perform test
+                tester = speedtest.Speedtest()
+                tester.get_servers(self.servers)
+                tester.get_best_server()
+                tester.download()
+                tester.upload(pre_allocate=False)
+                result = tester.results.dict()
+                
+                # Update metrics
+                metrics['speedtest_ping'].labels(server_name=result['server']['name'], server_loc=result['server']['country'], server_id=result['server']['id']).set(result['ping'])
+                metrics['speedtest_download'].labels(server_name=result['server']['name'], server_loc=result['server']['country'], server_id=result['server']['id']).set(result['download'])
+                metrics['speedtest_upload'].labels(server_name=result['server']['name'], server_loc=result['server']['country'], server_id=result['server']['id']).set(result['upload'])
+                print('INFO: Metrics updated!', flush=True)
+            except Exception:
+                # Set metrics to -1
+                metrics['speedtest_ping'].labels(server_name='', server_loc='', server_id=0).set(-1)
+                metrics['speedtest_download'].labels(server_name='', server_loc='', server_id=0).set(-1)
+                metrics['speedtest_upload'].labels(server_name='', server_loc='', server_id=0).set(-1)
+                print('ERROR: Unable to update metrics! Reason:\n{}'.format(traceback.print_exc()))
 
-def update_metrics():
-    global metrics, metrics_set
-    output = subprocess.Popen(speedtest_cmd.split(' '), stdout=subprocess.PIPE)
-    # stdout, stderr
-    raw_json, _ = output.communicate()
-    data = json.loads(raw_json.decode())
-
-    sponsor = data['server']['sponsor']
-    host = data['server']['host']
-    metrics['speedtest_ping'].set(data['ping'])
-    metrics['speedtest_download'].set(data['download'])
-    metrics['speedtest_upload'].set(data['upload'])
-    metrics_set = True
-
-
-def metrics_updater(_interval):
-    while True:
-        update_metrics()
-        time.sleep(_interval)
-
+            # Wait
+            time.sleep(self.interval)
 
 # Main
 if __name__ == '__main__':
-    # Handle arguments
-    interval = 10
+    print('INFO: Loading exporter...')
     options = parser.parse_args()
-    if options.server_id:
-        speedtest_cmd += ' --server {}'.format(options.server_id)
-    if options.bind_ip:
-        speedtest_cmd += ' --source {}'.format(options.bind_ip)
-    if options.interval:
-        interval = int(options.interval)
-
-    # Run tests
-    print('INFO: Performing test speedtest before running metrics updater...')
-    try:
-        subprocess.Popen(speedtest_cmd.split(' '), shell=True, stdout=subprocess.PIPE)
-    except:
-        print('ERROR: Invalid server id or IP to bind to!')
-        exit()
-    print('INFO: Tests complete!')
-    
-    # Start metrics updater
-    Thread(target=metrics_updater, args=(interval, )).start()
-
-    # Wait till initial metrics are set
-    print('INFO: Waiting for initial metrics to be set...')
-    while not metrics_set:
-        time.sleep(1)
-    print('INFO: Initial metrics are set! Proceeding to start HTTP service...')
-
-    # Start HTTP server
+    host = '0.0.0.0'
+    port = 9100
+    servers = []
+    source = None
+    interval = 900
     try:
         if options.listen_addr:
-            if len(options.listen_addr.split(':')) == 2:
-                ip = options.listen_addr.split(':')[0]
-                port = int(options.listen_addr.split(':')[-1])
-                print('INFO: Listening on {}:{}...'.format(ip, port))
-                start_http_server(port, addr=ip)
-            else:
-                print('ERROR: Invalid web.listen_address value! Must have IP and port separated by : !')
-                exit()
-        else:
-            print('INFO: Listening on {}:{}...'.format('0.0.0.0', 9100))
-            start_http_server(9100, addr='0.0.0.0')
-    except:
-        print('ERROR: Invalid IP for web.listen_address flag!')
-        exit()
+            host = options.listen_addr.split(':')[0]
+            port = int(options.listen_addr.split(':')[-1])
+        if options.servers:
+            if ',' in options.servers:
+                for server in options.servers.split(','):
+                    servers.append(int(server))
+        if options.source:
+            source = options.source
+        if options.interval:
+            interval = int(options.interval)
+    except Exception:
+        print('ERROR: Invalid argument input! Reason:\n{}'.format(traceback.print_exc()))
+    print('INFO: Exporter ready!')
+    UpdateMetrics(_servers=servers, _source=source, _interval=interval).start()
+    prometheus_client.start_http_server(port, host)
